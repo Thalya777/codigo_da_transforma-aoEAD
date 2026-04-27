458ff5e8ba6940f8f64db01ec84954ef73ebcca9
Thalya777 <thalyanaomy40@gmail.com>
2026-04-26 23:16:40 -0300

    update
    atualizaçoes do modulo 2

--- modulo_02/atividade_modulo02_thalya.py
+++ modulo_02/atividade_modulo02_thalya.py
@@ -1,5 +1,20 @@
-print('Olá, seja bem-vindo')
+'''
 
-nome_usuario = input('digite seu nome: ')
+atividade 2 
 
-print(f"Olá {nome_usuario}, seja bem-vindo!😎😎")
+'''
+
+
+
+print('Digite a idade do jovem: ')
+
+idade = int(input()) 
+
+if idade <=18:
+    print('jovem é menor de idade')
+
+elif idade <=65:
+      print('jovem é maior de idade')
+
+else:
+     print('Jovem é idoso de idade')
